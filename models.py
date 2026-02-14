89dbb2d983e10ef35cfff2eee75083127ddd7427
Ари <arianna2811@yandex.ru>
2026-02-14 16:08:14 +0300

    fix: add fields and str method

--- models.py
+++ models.py
@@ -8,3 +8,8 @@ class Task:
     deadline: str
     priority: str
     note: str
+    completed:bool=0
+    id:int=None
+
+    def __str__(self):
+        return f"{self.name,self.deadline,self.priority,self.note,self.completed}"
