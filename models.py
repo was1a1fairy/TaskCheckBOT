c5015dd5933db0ea82607df184192c25346e67f0
Ари <arianna2811@yandex.ru>
2026-05-06 22:32:27 +0300

    feat: User model

--- models.py
+++ models.py
@@ -13,3 +13,16 @@ class Task:
 
     def __str__(self):
         return f"{self.name,self.deadline,self.priority,self.note,self.completed}"
+
+
+@dataclass
+class User:
+
+    username:str
+    tg_id: str
+    email: str
+    password: str
+    id:int=None
+
+    def __str__(self):
+        return f"{self.username,self.tg_id,self.email,self.password}"
