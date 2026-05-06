d662d04d363d2ae6c5d8e959571d2c839cc33fa7
Ари <arianna2811@yandex.ru>
2026-05-06 22:35:54 +0300

    feat: include registration in polling

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from aiogram import Dispatcher
 import asyncio
 
 from secret_data import token
-import handlers.basic, handlers.main, handlers.optional
+import handlers
+import middleware
 
 
 
@@ -15,10 +16,11 @@ dp = Dispatcher()
 
 
 async def main():
-
+    dp.update.outer_middleware(middleware.register.Register)
     dp.include_routers(handlers.basic.router1)
     dp.include_routers(handlers.main.router2)
     dp.include_routers(handlers.optional.router3)
+    dp.include_routers(handlers.register.router4)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
